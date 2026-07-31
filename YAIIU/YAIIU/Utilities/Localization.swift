8e623467d3746170f33808c2eaaf1830482628e1
FawenYo <40032648+FawenYo@users.noreply.github.com>
2026-07-31 16:56:39 +0800

    feat: Validate Immich URL when using background upload (#68)
    * feat: Check if Immich URL is set to immich-proxy when enabling background upload
    
    * fix: Use appendingPathComponent for safe health URL construction
    
    Avoids double-slash URLs when serverURL ends with a trailing slash.

--- YAIIU/YAIIU/Utilities/Localization.swift
+++ YAIIU/YAIIU/Utilities/Localization.swift
@@ -430,6 +430,7 @@ enum L10n {
         static var notSupported: String { "backgroundUpload.notSupported".localized }
         static var errorPhotoLibraryNotAuthorized: String { "backgroundUpload.error.photoLibraryNotAuthorized".localized }
         static var errorNotLoggedIn: String { "backgroundUpload.error.notLoggedIn".localized }
+        static var errorNotUsingImmichProxy: String { "backgroundUpload.error.notUsingImmichProxy".localized }
         static var errorExtensionNotAvailable: String { "backgroundUpload.error.extensionNotAvailable".localized }
         static var lastUploadLabel: String { "backgroundUpload.lastUploadLabel".localized }
         static var neverUploaded: String { "backgroundUpload.neverUploaded".localized }
