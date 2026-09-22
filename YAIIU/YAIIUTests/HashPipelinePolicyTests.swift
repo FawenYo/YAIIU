1f79c0ca2f53d769e3baaac4e8c481d3f7d1386f
FawenYo <40032648+FawenYo@users.noreply.github.com>
2026-09-23 00:34:39 +0200

    test: pin streaming hash batch size to 32

--- YAIIU/YAIIUTests/HashPipelinePolicyTests.swift
+++ YAIIU/YAIIUTests/HashPipelinePolicyTests.swift
@@ -254,6 +254,10 @@ final class HashPipelinePolicyTests: XCTestCase {
         XCTAssertFalse(state.owns(finalRunID))
     }
 
+    func testStreamingHashBatchSizeMatchesImmichExperiment() {
+        XCTAssertEqual(HashPipelinePolicy.streamingHashBatchSize, 32)
+    }
+
     func testPhotoKitRateLimitUsesKnownEstimate() {
         let estimate: Int64 = 42 * 1024 * 1024
         XCTAssertEqual(
