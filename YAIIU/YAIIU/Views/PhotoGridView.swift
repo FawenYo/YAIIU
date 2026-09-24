53c4d59d3035964edb1e83a7f08a2807e99bd5c5
FawenYo <40032648+FawenYo@users.noreply.github.com>
2026-09-24 18:38:44 +0200

    perf: start background processing from lightweight snapshots

--- YAIIU/YAIIU/Views/PhotoGridView.swift
+++ YAIIU/YAIIU/Views/PhotoGridView.swift
@@ -546,9 +546,9 @@ struct PhotoGridView: View {
         
         let manager = photoLibraryManager
         Task.detached(priority: .utility) {
-            let assets = manager.allAssets()
+            let snapshots = manager.allAssetSnapshots()
             await MainActor.run {
-                HashManager.shared.startBackgroundProcessing(assets: assets)
+                HashManager.shared.startBackgroundProcessing(snapshots: snapshots)
             }
         }
     }
